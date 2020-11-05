502c0ee6b3a71ec58e064cd4d7495739712637e2
Hendrik Rakemann <hendrik.rakemann@gmx.de>
2020-11-05 23:14:17 +0100

    Initial Commit

---
+++ Game/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject m_StartMenu;
+    [SerializeField] private GameObject m_SelectPlayMode;
+
+    public void BtnPlay_Click()
+    {
+        this.m_StartMenu.SetActive(false);
+        this.m_SelectPlayMode.SetActive(true);        
+    }
+
+    public void BtnQuit_Click()
+    {
+        Application.Quit();
+    }
+
+    public void BtnHowTo_Click()
+    {
+
+    }
+
+    public void BtnCredits_Click()
+    {
+
+    }
+
+    public void BtnStoryMode_Click()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void BtnRandomMode_Click()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void BtnBackToStartScreen_Click()
+    {
+        this.m_SelectPlayMode.SetActive(false); 
+        this.m_StartMenu.SetActive(true);        
+    }
+}
