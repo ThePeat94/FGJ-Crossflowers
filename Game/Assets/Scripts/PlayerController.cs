b06d62773361c0b881b23c94864ade63f4aa738c
Hendrik Rakemann <hendrik.rakemann@gmx.de>
2020-11-08 12:33:16 +0100

    bug fixes sfx

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -5,24 +5,16 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using Scriptables;
 using UI;
-using UnityEditor.Rendering;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private static PlayerController s_instance;
-    
-    [SerializeField] private float m_movementSpeed;
-    [SerializeField] private float m_rotationSpeed;
-    [SerializeField] private float m_movementStaminaCost;
-    [SerializeField] private ResourceData m_staminaData;
-    [SerializeField] private ResourceData m_waterData;
+
+    [SerializeField] private PlayerData m_playerData;
     [SerializeField] private GameObject m_rake;
     [SerializeField] private GameObject m_waterCan;
-    [SerializeField] private float m_ploughStaminaCost;
-    [SerializeField] private float m_gatherStaminaCost;
-    [SerializeField] private float m_waterStaminaCost;
-    [SerializeField] private float m_waterFieldCost;
+
     
     private ResourceController m_staminaController;
     private ResourceController m_waterController;
@@ -50,8 +42,9 @@ public class PlayerController : MonoBehaviour
     public void PlantSeed(Seed seed)
     {
         this.m_playerInventory.RemoveSeed(seed);
-        this.m_staminaController.UseResource(this.m_ploughStaminaCost);
+        this.m_staminaController.UseResource(this.m_playerData.PloughStaminaCost);
         this.StartCoroutine(this.PlayPloughAnimation());
+        AudioSource.PlayClipAtPoint(this.m_playerData.RakingSoundEffect, Camera.main.transform.position, 0.33f);
     }
     
     private void Awake()
@@ -70,8 +63,8 @@ public class PlayerController : MonoBehaviour
         this.m_inputProcessor = this.GetComponent<InputProcessor>();
         this.m_characterController = this.GetComponent<CharacterController>();
         this.m_animator = this.GetComponent<Animator>();
-        this.m_staminaController = new ResourceController(this.m_staminaData);
-        this.m_waterController = new ResourceController(this.m_waterData);
+        this.m_staminaController = new ResourceController(this.m_playerData.StaminaData);
+        this.m_waterController = new ResourceController(this.m_playerData.WaterData);
     }
     
     // Update is called once per frame
@@ -130,11 +123,11 @@ public class PlayerController : MonoBehaviour
     protected void Move()
     {
         this.m_moveDirection = new Vector3(-this.m_inputProcessor.Movement.y, Physics.gravity.y, this.m_inputProcessor.Movement.x);
-        this.m_characterController.Move(this.m_moveDirection * Time.deltaTime * this.m_movementSpeed);
+        this.m_characterController.Move(this.m_moveDirection * Time.deltaTime * this.m_playerData.MovementSpeed);
 
         if (this.m_moveDirection != Physics.gravity)
         {
-            this.m_staminaController.UseResource(this.m_movementStaminaCost);
+            this.m_staminaController.UseResource(this.m_playerData.MovementStaminaCost);
         }
     }
         
@@ -179,27 +172,27 @@ public class PlayerController : MonoBehaviour
 
     private void InteractWithField(Field field)
     {
-        if (field.CanHarvestFlower() && this.m_staminaController.UseResource(this.m_gatherStaminaCost))
+        if (field.CanHarvestFlower() && this.m_staminaController.UseResource(this.m_playerData.GatherStaminaCost))
         {
             field.Interact();
             this.StartCoroutine(this.PlayGatherAnimation());
         }
-        else if (field.CanWaterField() && this.m_staminaController.CanAfford(this.m_waterStaminaCost) && this.m_waterController.CanAfford(this.m_waterFieldCost))
+        else if (field.CanWaterField() && this.m_staminaController.CanAfford(this.m_playerData.WaterStaminaCost) && this.m_waterController.CanAfford(this.m_playerData.WaterFieldCost))
         {
             field.Interact();
             this.StartCoroutine(this.PlayWaterAnimation());
-            this.m_staminaController.UseResource(this.m_waterStaminaCost);
-            this.m_waterController.UseResource(this.m_waterFieldCost);
+            this.m_staminaController.UseResource(this.m_playerData.WaterStaminaCost);
+            this.m_waterController.UseResource(this.m_playerData.WaterFieldCost);
         }
-        else if (field.CanPlantSeed() && this.m_staminaController.CanAfford(this.m_ploughStaminaCost) && this.m_playerInventory.Seeds.Count > 0)
+        else if (field.CanPlantSeed() && this.m_staminaController.CanAfford(this.m_playerData.PloughStaminaCost) && this.m_playerInventory.Seeds.Count > 0)
         {
             field.Interact();
         }
         else
         {
             StartCoroutine(this.PlayShrugAnimation());
-            if (!this.m_staminaController.CanAfford(this.m_gatherStaminaCost) || !this.m_staminaController.CanAfford(this.m_waterStaminaCost) ||
-                !this.m_staminaController.CanAfford(this.m_ploughStaminaCost))
+            if (!this.m_staminaController.CanAfford(this.m_playerData.GatherStaminaCost) || !this.m_staminaController.CanAfford(this.m_playerData.WaterStaminaCost) ||
+                !this.m_staminaController.CanAfford(this.m_playerData.PloughStaminaCost))
             {
                 PlayerHudUI.Instance.ShowPlayerMonologue("I am too exhausted to work anymore. I need some rest.");
             }
@@ -207,7 +200,7 @@ public class PlayerController : MonoBehaviour
             {
                 PlayerHudUI.Instance.ShowPlayerMonologue("This field is watered and has a planted seed. I should give it a day to grow.");
             }
-            else if (!field.IsWatered && !this.m_waterController.CanAfford(this.m_waterFieldCost))
+            else if (!field.IsWatered && !this.m_waterController.CanAfford(this.m_playerData.WaterFieldCost))
             {
                 PlayerHudUI.Instance.ShowPlayerMonologue("My water can can't fulfill the needs of this field! I need to fill it up again.");
             }
@@ -246,7 +239,9 @@ public class PlayerController : MonoBehaviour
         this.m_inputProcessor.enabled = false;
         this.m_waterCan.SetActive(true);
         this.m_animator.SetTrigger(s_waterAnimationTriggerHash);
-        yield return new WaitForSeconds(5.583f);
+        yield return new WaitForSeconds(1.5f);
+        AudioSource.PlayClipAtPoint(this.m_playerData.WateringSoundEffect, Camera.main.transform.position, 0.33f);
+        yield return new WaitForSeconds(1.5f);
         this.m_waterCan.SetActive(false);
         this.m_inputProcessor.enabled = true;
     }
@@ -264,7 +259,7 @@ public class PlayerController : MonoBehaviour
     private void RotateTowards(Vector3 dir)
     {
         var lookRotation = Quaternion.LookRotation(dir.normalized);
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRotation, this.m_rotationSpeed * Time.deltaTime);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRotation, this.m_playerData.RotationSpeed * Time.deltaTime);
     }
 
     private void UpdateAnimator()
